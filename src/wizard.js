d3045203e75c55f557285a14de6a22edfa086996
Ivan Santos <pragmaticivan@gmail.com>
2017-02-16 18:15:17 -0800

    Adds support to external function to process module actions

--- src/wizard.js
+++ src/wizard.js
@@ -24,28 +24,49 @@ class Wizard {
     this.injection_ = [];
     this.exclusion_ = [];
 
+    this.fnProcessor_ = this.defaultProcessor_;
+
     this.loadDefaultExclusion_();
 
     this.log_(['Initialized in', this.getOptions().cwd]);
   }
 
   /**
-   * Accumulate glob pattern to be inserted into the main object.
-   * @param  {string} glob
-   * @return {Wizard}
+   * Creates namespace.
+   * @param  {Object} parent
+   * @param  {Array} parts
+   * @param  {Function|Object} mod
+   * @return {Object}
    */
-  inject(glob) {
-    if (!glob) {
-      throw new Error('Glob is required.');
+  createNamespace_(parent, parts, mod) {
+    let part = parts.shift();
+
+    if (!parent[part]) {
+      parent[this.getKeyName_(part)] = parts.length ? {} : mod;
     }
 
-    if (Array.isArray(glob)) {
-      this.getInjection().push(glob);
-    } else {
-      this.getInjection().push([glob]);
+    if (parts.length) {
+      parent = this.createNamespace_(parent[part], parts, mod);
     }
 
-    return this;
+    return parent;
+  }
+
+  /**
+   * Default processor to perform call of args in a function.
+   * @param {Function|Object} mod
+   * @param {Array} optArgs
+   * @return {Function|Object}
+   */
+  defaultProcessor_(mod, optArgs) {
+    if (mod.default) {
+      mod = mod.default;
+    }
+
+    if (typeof mod === 'function') {
+      mod = mod.apply(mod, optArgs);
+    }
+    return mod;
   }
 
   /**
@@ -68,22 +89,27 @@ class Wizard {
   }
 
   /**
-   * Merge Options.
-   * @param  {Object} optConfig;
+   * Returns module manipulation processor.
+   * @return {Function}
    */
-  mergeOptions_(optConfig) {
-    this.options_ = Object.assign(this.options_, optConfig);
+  getProcessor() {
+    return this.fnProcessor_;
   }
 
   /**
-   * Load default glob for exclusion.
+   * Accumulate glob pattern to be inserted into the main object.
+   * @param  {string} glob
    * @return {Wizard}
    */
-  loadDefaultExclusion_() {
-    const defaultExclusion = this.getOptions().defaultExclusion;
+  inject(glob) {
+    if (!glob) {
+      throw new Error('Glob is required.');
+    }
 
-    if (defaultExclusion.length > 0) {
-      return this.exclude(defaultExclusion);
+    if (Array.isArray(glob)) {
+      this.getInjection().push(glob);
+    } else {
+      this.getInjection().push([glob]);
     }
 
     return this;
@@ -109,33 +135,58 @@ class Wizard {
   }
 
   /**
+   * Load default glob for exclusion.
+   * @return {Wizard}
+   */
+  loadDefaultExclusion_() {
+    const defaultExclusion = this.getOptions().defaultExclusion;
+
+    if (defaultExclusion.length > 0) {
+      return this.exclude(defaultExclusion);
+    }
+
+    return this;
+  }
+
+  /**
+   * Logs using default logger
+   * @param  {string} message
+   * @param  {string} type
+   * @return {Wizard}
+   */
+  log_(message, type) {
+    if (this.getOptions().verbose) {
+      this.getOptions()
+          .logger[type || this.getOptions().loggingType](message.join(' '));
+    }
+
+    return this;
+  }
+
+  /**
+   * Merge Options.
+   * @param  {Object} optConfig;
+   */
+  mergeOptions_(optConfig) {
+    this.options_ = Object.assign(this.options_, optConfig);
+  }
+
+  /**
    * Process file injection.
    * @param  {string[]} fileGroup
    * @param  {Object} obj
    * @param  {string[]} optArgs
    */
   processInjection_(fileGroup, obj, optArgs) {
-    fileGroup.forEach((f) => {
-      let loopFile = f;
-
+    fileGroup.forEach((loopFile) => {
       delete require.cache[this.getFullPath_(loopFile)];
 
-      let args = [];
       let parts = this.getRelativePath_(loopFile).split(path.sep).slice(1);
       let mod = require(this.getFullPath_(loopFile));
 
-      if (mod.default) {
-        mod = mod.default;
-      }
-
-      args.push(obj);
-
-      optArgs.forEach((arg) => {
-        args.push(arg);
-      });
-
-      if (typeof mod === 'function') {
-        mod = mod.apply(mod, args);
+      if(this.getProcessor()) {
+        let processor = this.getProcessor();
+        mod = processor(mod, optArgs);
       }
 
       this.log_(['+', this.getRelativePath_(loopFile)], 'info');
@@ -145,24 +196,11 @@ class Wizard {
   }
 
   /**
-   * Creates namespace.
-   * @param  {Object} parent
-   * @param  {Array} parts
-   * @param  {Function|Object} mod
-   * @return {Object}
+   * Sets module manipulation processor.
+   * @param {Function} fnProcessor
    */
-  createNamespace_(parent, parts, mod) {
-    let part = parts.shift();
-
-    if (!parent[part]) {
-      parent[this.getKeyName_(part)] = parts.length ? {} : mod;
-    }
-
-    if (parts.length) {
-      parent = this.createNamespace_(parent[part], parts, mod);
-    }
-
-    return parent;
+  setProcessor(fnProcessor) {
+    this.fnProcessor_ = fnProcessor;
   }
 
   /**
@@ -195,6 +233,15 @@ class Wizard {
     }
 
     return groupedFiles;
+  }
+
+  /**
+   * Get full file path.
+   * @param  {string} file
+   * @return {string}
+   */
+  getFullPath_(file) {
+    return `${this.getOptions().cwd}/${file}`;
   }
 
   /**
@@ -232,30 +279,6 @@ class Wizard {
    */
   getOptions() {
     return this.options_;
-  }
-
-  /**
-   * Get full file path.
-   * @param  {string} file
-   * @return {string}
-   */
-  getFullPath_(file) {
-    return `${this.getOptions().cwd}/${file}`;
-  }
-
-  /**
-   * Logs using default logger
-   * @param  {string} message
-   * @param  {string} type
-   * @return {Wizard}
-   */
-  log_(message, type) {
-    if (this.getOptions().verbose) {
-      this.getOptions()
-          .logger[type || this.getOptions().loggingType](message.join(' '));
-    }
-
-    return this;
   }
 
   /**
